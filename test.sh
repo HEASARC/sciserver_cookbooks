4a1956bad3df0393ff9f63e4e13d19d5dfde726a
TR Jaffe <trjaffe@gmail.com>
2024-03-12 14:55:31 -0400

    Better logging and error trapping

--- test.sh
+++ test.sh
@@ -2,16 +2,31 @@
 
 for nb in *.md; 
 do
-        if [ "$nb" = "README.md" ] || [ "$nb" = "introduction.md" ] || [ ${nb} = "CHANGES.md" ] ; then 
-                continue
-        fi 
-        nbroot=$(echo "${nb}" | cut -d. -f1)
-        echo "Processing ${nbroot}"
-        mkdir "${nbroot}.out" || echo "Outdir ${nbroot}.out already exists?"
-        cd "${nbroot}.out"
-        cp "../${nb}" .
-        jupytext --to notebook "${nb}" "${nbroot}.ipynb"
-        jupyter nbconvert --to notebook --execute "${nbroot}.ipynb"  > output.log 2>&1 || (echo "Exit Code: $?" | tee -ai output.log)
-
+    if [ "$nb" = "README.md" ] || [ "$nb" = "introduction.md" ] || [ ${nb} = "CHANGES.md" ] ; then 
+        continue
+    fi 
+    nbroot=$(echo "${nb}" | cut -d. -f1)
+    echo "================\nProcessing ${nbroot}"
+    date
+    if [ ! -d "${nbroot}.out" ]; then
+        mkdir "${nbroot}.out"
+    fi
+    cd "${nbroot}.out"
+    if [ -f "00_success" ]; then
+        echo "Found previous successful run;  skipping" 
         cd ..
+        continue
+    elif [ -f "00_failure" ]; then 
+        echo "Found previous failed run;  skipping" 
+        cd ..
+        continue
+    fi
+    cp "../${nb}" . || (echo "Cannot find ../${nb}; exiting" ; exit 1)
+    # There's probably a way to combine these?
+    jupytext --to notebook "${nb}" "${nbroot}.ipynb" > output.log 2>&1 || (echo "ERROR:  Exit Code: $?" ; tail output.log ; touch 00_failure; exit 1)
+    jupyter nbconvert --to notebook --execute "${nbroot}.ipynb"  >> output.log 2>&1 || (echo "ERROR:  Exit Code: $?" ; tail output.log ; touch 00_failure ; exit 1)
+    touch 00_success
+    cd ..
 done
+date
+echo "\n============\nDone"
